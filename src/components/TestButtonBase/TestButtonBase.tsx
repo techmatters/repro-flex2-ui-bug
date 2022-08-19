05d2ef4862094c1c2775a6c6941ed9252ffd0914
Stephen Hand <steveh@techmatters.org>
2022-08-19 13:51:40 +0100

    Wrap the test material button in a StylesProvider that sets it to inject its styles first

--- src/components/TestButtonBase/TestButtonBase.tsx
+++ src/components/TestButtonBase/TestButtonBase.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { ButtonBase, withStyles } from '@material-ui/core';
+import { StylesProvider } from '@material-ui/core/styles';
 
 const StyledButton = withStyles({
   root: {
+    backgroundColor: 'red',
     width: 200,
     height: 40,
   },
 })(ButtonBase);
 
-const TestButtonBase = () => <StyledButton onClick={() => console.log('Clicked!')}>Some button</StyledButton>
+const TestButtonBase = () => <StylesProvider injectFirst={true}><StyledButton onClick={() => console.log('Clicked!')}>Some button</StyledButton></StylesProvider>
 
 export default TestButtonBase;
